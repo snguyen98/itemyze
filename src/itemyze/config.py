87fece6754ffc887fb3c7864a7dd859e1fc1b21c
Sang Nguyen <snguyen.cs@outlook.com>
2024-08-14 17:10:53 +0100

    Delete src/itemyze/config.py

--- src/itemyze/config.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-    SW_API_KEY = 'L6ubrOwTIhiKe9C1YxJnU7aegMsR9MsE34W0I4F9'
-    SW_API_SECRET = 'TjCckur1taZE1nWSlXZgyOuRxa9zT3FDcpPKxbq0'
-
-    SW_TOKEN_URL = 'https://secure.splitwise.com/oauth/token'
