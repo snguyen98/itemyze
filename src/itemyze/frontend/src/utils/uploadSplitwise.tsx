33ef399e586ba6ec066b06e70123429fc7561de8
snguyen98 <snguyen.cs@outlook.com>
2025-04-06 23:14:44 +0100

    Removed unnecessary code from uploadSplitwise util

--- src/itemyze/frontend/src/utils/uploadSplitwise.tsx
+++ src/itemyze/frontend/src/utils/uploadSplitwise.tsx
@@ -1,14 +1,7 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
-import * as changeCase from "change-case";
-import Dict from "../interfaces/Dict";
 
 async function uploadSplitwise(expenseId: number) {
-    const camelize = (data: Dict) => Object.keys(data).reduce((acc: Dict, key: string) => {
-        acc[changeCase.camelCase(key)] = data[key]
-        return acc
-    }, {});
-
     const csrftoken = Cookies.get('csrftoken');
 
     try {
