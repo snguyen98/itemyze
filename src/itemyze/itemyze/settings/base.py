bee542321a69199757a83a84c8e0d21e09fe6b14
snguyen98 <snguyen.cs@outlook.com>
2025-04-07 21:45:37 +0100

    Added auth to api

--- src/itemyze/itemyze/settings/base.py
+++ src/itemyze/itemyze/settings/base.py
@@ -55,6 +55,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 # Configure SimpleJWT settings
