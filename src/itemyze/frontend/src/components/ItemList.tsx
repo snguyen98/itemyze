a6d2ffe4979c2bc355446b0f6c9f7985e9cde1f3
snguyen98 <snguyen.cs@outlook.com>
2025-04-11 16:31:25 +0100

    Added delete button (UI element)

--- src/itemyze/frontend/src/components/ItemList.tsx
+++ src/itemyze/frontend/src/components/ItemList.tsx
@@ -7,28 +7,44 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function ItemList({ items, currency, onItemSelect }: { items: Item[],  currency: string, onItemSelect: (item: Item) => void }) {
+interface AllocateItemsProps {
+    items: Item[];
+    currency: string;
+    onItemSelect: (item: Item) => void;
+}
+
+function ItemList({ items, currency, onItemSelect }: AllocateItemsProps) {
     return (
-        <div className="content">
+        <>
             <TableContainer component={Paper}>
                 <Table>
                     <TableBody>
                         { items.map((item, row) => (
-                            <TableRow key={row} hover onClick={() => onItemSelect(item)}>
+                            <TableRow key={row} hover>
                                 <TableCell>{ item.name }</TableCell>
                                 <TableCell>{ currency + item.cost }</TableCell>
-                                <TableCell><EditIcon fontSize="medium" /></TableCell>
+                                <TableCell>
+                                    <IconButton onClick={() => onItemSelect(item)}>
+                                        <EditIcon fontSize="medium" />
+                                    </IconButton>
+                                </TableCell>
+                                <TableCell>
+                                    <IconButton>
+                                        <DeleteIcon fontSize="medium" />
+                                    </IconButton>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-        </div>
+        </>
     );
 }
 
