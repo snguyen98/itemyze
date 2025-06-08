9b5aa63129d203061e751e79ec76f688482e4032
snguyen98 <snguyen.cs@outlook.com>
2025-06-08 15:52:20 +0100

    Added WhiteNoise middleware

--- src/backend/itemyze/itemyze/settings/base.py
+++ src/backend/itemyze/itemyze/settings/base.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # Add custom middleware
     "token_auth.middleware.jwt_cookie_middleware.JWTCookieMiddleware",
 ]
